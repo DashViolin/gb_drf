0f93b850a12689e1363070bc8225b462939b9d4e
DashViolin <mymail@srv.org>
2022-08-05 19:49:57 +0300

    lesson_02: simple react frontend + some minor backand changes

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+const Header = () => {
+  return (
+    <div className="container">
+      <h1 className="mt-5">Users table</h1>
+      <p className="lead">Content got from Django REST Framework backend.</p>
+    </div>
+  )
+}
+
+export default Header
--- userapp/serializers.py
+++ userapp/serializers.py
@@ -8,4 +8,4 @@ from .models import CustomUser
 class UserModelSerializer(ModelSerializer):
     class Meta:
         model = CustomUser
-        fields = ["username", "first_name", "last_name", "email"]
+        fields = ["id", "username", "first_name", "last_name", "email"]
